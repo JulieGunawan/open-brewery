7fe0e2ad64c19b5f68f0828bbb8fc64f7a6b4b67
Simon <29654835+absolutelysimon@users.noreply.github.com>
2021-09-25 11:35:42 -0600

    Mob coding test!

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Brewery from "./components/Brewery";
 
+//Mob programming test!
+
 function App() {
   let [data, setData] = useState();
   let [breweryId, setId] = useState(
