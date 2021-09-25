460d712313069ddba2ec407311295c82cc0828ec
Simon <29654835+absolutelysimon@users.noreply.github.com>
2021-09-25 15:54:59 -0600

    Major CSS improvements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './beer.png'
-import './App.css'
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-import { Router, Link } from '@reach/router'
-import { Brewery } from './components'
-import { City } from './pages/City'
-import { RandomBrewery } from './pages/RandomBrewery'
+import logo from "./beer.png";
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Router, Link } from "@reach/router";
+import { Brewery } from "./components";
+import { City } from "./pages/City";
+import { RandomBrewery } from "./pages/RandomBrewery";
 
 //Mob programming test!
 
@@ -14,9 +14,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Simon's Super Cool Brewery Website</h1>
-        <Link to="city">City</Link>
-        <Link to="/">Random Brewery</Link>
+        <h1>Our Super Cool Brewery Website</h1>
+        <div className="Header-links">
+          <Link to="city">City</Link>
+          <Link to="/">Random Brewery</Link>
+        </div>
       </header>
       <body>
         <Router className="infoCard">
@@ -25,7 +27,7 @@ function App() {
         </Router>
       </body>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
