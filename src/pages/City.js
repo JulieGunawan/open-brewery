9642d8a2a3134b2fe61091d4d6481c25a2a0bc3e
Simon <29654835+absolutelysimon@users.noreply.github.com>
2021-09-25 14:30:47 -0600

    Midway through city search

--- src/pages/City.js
+++ src/pages/City.js
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export function City() {
-
-  let [data, setData] = useState();
-  let [city, setCity] = useState();
+  let [data, setData] = useState(null);
+  let [city, setCity] = useState(null);
 
   useEffect(() => {
     async function getData() {
@@ -15,10 +14,17 @@ export function City() {
     }
     getData();
   }, [city]);
+  debugger;
 
-  return data ? <div><City info={data} />
-    <input onChange={(value) => {
-      setCity(value)
-    }} />
-  </div> : "City not found";
+  return data.length == 0 ? (
+    <div>
+      <input
+        onChange={(event) => {
+          setCity(event.target.value);
+        }}
+      />
+    </div>
+  ) : (
+    "City not found"
+  );
 }
