f74275a6820a1f6602642102ad8afa920c5acb30
Simon <29654835+absolutelysimon@users.noreply.github.com>
2021-09-24 18:37:50 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./beer.png";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
+  let [data, setData] = useState();
+  let [breweryId, setId] = useState(Math.floor(Math.random() * 10000) + 1);
+  // const results = await axios.get(
+  //   "https://api.openbrewerydb.org/breweries/" + breweryId
+  // );
+  // setData(results.data);
+  // if (!data){
+  //   setId(Math.floor(Math.random() * 10000) + 1);
+  // }
+  useEffect(() => {
+    async function getData() {
+      try {
+        const results = await axios.get(
+          "https://api.openbrewerydb.org/breweries/" + breweryId
+        );
+        setData(results.data);
+      } catch (err) {
+        setId(Math.floor(Math.random() * 10000) + 1);
+      }
+      // debugger;
+    }
+    getData();
+    // getData();
+  }, [breweryId]);
+  // useEffect(() => {
+  //   if (!data) {
+  //     setId(Math.floor(Math.random() * 10000) + 1);
+  //   }
+  // }, []);
+  return data ? (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        Name: {data.name}
+        <br />
+        Type: {data.brewery_type}
+        <br />
+        City: {data.city} <br />
+        <a href={data.website_url}>Brewery Website</a>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={
+            "https://maps.google.com/?q=" + data.latitude + "," + data.longitude
+          }
         >
-          Learn React
+          Google Maps Link
         </a>
+        <br />
       </header>
+      <button onClick={() => setId(Math.floor(Math.random() * 10000) + 1)}>
+        New Brewery
+      </button>
     </div>
+  ) : (
+    "Loading..."
   );
 }
 
